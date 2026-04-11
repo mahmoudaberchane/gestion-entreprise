a7b1de14de3e3a46584b7935538997ef343d5913
dev1-chef <dev1-chef@exp.com>
2026-04-11 15:15:51 +0100

    init: creation du projet Spring Boot

---
+++ src/test/java/ma/gestion_entreprise/GestionEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.gestion_entreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
